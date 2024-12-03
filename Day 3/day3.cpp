eaefd75f2a68514004fd0e3f5fe64afb01162b95
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2024-12-03 17:00:50 -0500

    cleanup and optimizations

--- Day 3/day3.cpp
+++ Day 3/day3.cpp
@@ -20,20 +20,22 @@ string read_file( const string& fileName) {
     return result;
 }
 int part1_total(string file_contents) {
-    // default constructor = end-of-sequence:
-    std::regex_token_iterator<std::string::iterator> end;
 
-    std::regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\))");
-    std::regex_token_iterator<std::string::iterator> a ( file_contents.begin(), file_contents.end(), mul_regex );
+    //parse out tokens with regex
+    const regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\))");
+    regex_token_iterator regex_iterator ( file_contents.begin(), file_contents.end(), mul_regex );
+    regex_token_iterator<string::iterator> end; // default constructor = end-of-sequence:
     vector<string> tokens;
-    while (a!=end) { tokens.push_back(a++->str()); }
+    while (regex_iterator!=end) tokens.push_back(regex_iterator++->str());
+
     int total = 0;
     for (auto token : tokens) {
-        const size_t substring_start = token.find("(") + 1;
-        const unsigned long substring_length = token.size() - substring_start - 1;
         //string manipulation
+        const size_t substring_start = token.find("(") + 1; //lop off mul(
+        const unsigned long substring_length = token.size() - substring_start - 1; // lop off )
         token = token.substr( substring_start , substring_length );
         token.replace(token.find(','),1,1,' ');
+
         // output
         istringstream iss(token) ;
         int x, y; iss >> x >> y;
@@ -43,34 +45,37 @@ int part1_total(string file_contents) {
 }
 
 int part2_total(string file_contents) {
-    // default constructor = end-of-sequence:
-    std::regex_token_iterator<std::string::iterator> end;
 
-    std::regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don't\(\))");
-    std::regex_token_iterator<std::string::iterator> a ( file_contents.begin(), file_contents.end(), mul_regex );
+    //parse out tokens with regex
+    const regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don't\(\))");
+    regex_token_iterator regex_iterator ( file_contents.begin(), file_contents.end(), mul_regex );
+    regex_token_iterator<string::iterator> end; // default constructor = end-of-sequence:
     vector<string> tokens;
-    while (a!=end) { tokens.push_back(a++->str()); }
+    while (regex_iterator!=end) tokens.push_back(regex_iterator++->str());
+
     int total = 0;
     bool mul_enabled = true;
     for (auto token : tokens) {
-        const size_t substring_start = token.find("(") + 1;
-        const unsigned long substring_length = token.size() - substring_start - 1;
-        //string manipulation
-        token = token.substr( substring_start , substring_length );
-        token.replace(token.find(','),1,1,' ');
-        // output
-        istringstream iss(token) ;
-        int x, y; iss >> x >> y;
-        total += x*y;
+        if (token == "do()") { mul_enabled = true; continue; }
+        if (token == "don't()") { mul_enabled = false; continue; }
+        if (mul_enabled) {
+            //string manipulation
+            const size_t substring_start = token.find("(") + 1;
+            const unsigned long substring_length = token.size() - substring_start - 1;
+            token = token.substr( substring_start , substring_length );
+            token.replace(token.find(','),1,1,' ');
+            // output
+            istringstream iss(token) ;
+            int x, y; iss >> x >> y;
+            total += x*y;
+        }
     }
     return total;
 }
 int main() {
     const string fileName = "Day 3/day3.txt";
-
-    const std::string str = read_file(fileName);
-    cout << "total: " << part1_total(str) << endl;
-    cout << "total part2: " << part2_total(str) << endl;
+    const string fileContents = read_file(fileName);
+    cout << "part 1 total: " << part1_total(fileContents) << endl;
+    cout << "part 2 total: " << part2_total(fileContents) << endl;
     return 0;
-
 }
