042cc4b01f9ee38364e7f441cb77b06425f93592
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2024-12-03 11:15:35 -0500

    solved pt 2

--- Day 3/day3.cpp
+++ Day 3/day3.cpp
@@ -22,17 +22,12 @@ string read_file( const string& fileName) {
     }
     return result;
 }
-
-int main() {
-    const string fileName = "Day 3/day3.txt";
-
-    std::regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\))");
-    std::string str = read_file(fileName);
-
+int part1_total(string file_contents) {
     // default constructor = end-of-sequence:
     std::regex_token_iterator<std::string::iterator> end;
 
-    std::regex_token_iterator<std::string::iterator> a ( str.begin(), str.end(), mul_regex );
+    std::regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\))");
+    std::regex_token_iterator<std::string::iterator> a ( file_contents.begin(), file_contents.end(), mul_regex );
     vector<string> tokens;
     while (a!=end) {
         tokens.push_back(a++->str());
@@ -45,9 +40,42 @@ int main() {
         istringstream iss(token);
         int x, y; iss >> x >> y;
         total += x*y;
-
     }
-    cout << "total: " << total << endl;
+    return total;
+}
+
+int part2_total(string file_contents) {
+    // default constructor = end-of-sequence:
+    std::regex_token_iterator<std::string::iterator> end;
+
+    std::regex mul_regex(R"(mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don't\(\))");
+    std::regex_token_iterator<std::string::iterator> a ( file_contents.begin(), file_contents.end(), mul_regex );
+    vector<string> tokens;
+    while (a!=end) {
+        tokens.push_back(a++->str());
+    }
+    int total = 0;
+    bool mul_enabled = true;
+    for (auto token : tokens) {
+        if (token == "do()") {  mul_enabled = true; continue;}
+        if (token == "don't()") {  mul_enabled = false; continue;}
+        if (mul_enabled) {
+            token.erase(token.begin(), token.begin()+4);
+            token.erase(token.end()-1, token.end());
+            token.replace(token.find(','),1,1,' ');
+            istringstream iss(token);
+            int x, y; iss >> x >> y;
+            total += x*y;
+        }
+    }
+    return total;
+}
+int main() {
+    const string fileName = "Day 3/day3.txt";
+
+    std::string str = read_file(fileName);
+    cout << "total: " << part1_total(str) << endl;
+    cout << "total part2: " << part2_total(str) << endl;
     return 0;
 
 }
